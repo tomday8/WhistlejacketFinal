64b4414ff5084d7128264f580c5b308c37ea30a3
Tom Day <111976454+tomday8@users.noreply.github.com>
2023-01-25 17:07:36 +0000

    clean-up, removed unused styles, tests passing

--- tests/test_index_route.py
+++ tests/test_index_route.py
@@ -1,13 +1,14 @@
 import unittest
-
 from app import app
+
+# Run tests: python3 -m unittest discover -s tests/
 
 class ContentOfIndex(unittest.TestCase):
     def test_index_body(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'<h1>Welcome to the movie randomizer</h1>' in response.data)
+        self.assertTrue(b'<h1>Welcome to The Movie Randomizer</h1>' in response.data)
 
 if __name__ == "__main__":
    unittest.main()
--- tests/test_movies_route.py
+++ tests/test_movies_route.py
@@ -3,12 +3,14 @@ from app import app, return_movie, conn
 import subprocess
 from unittest.mock import MagicMock
 
+# Run tests: python3 -m unittest discover -s tests/
+
 class ContentOfMovies(unittest.TestCase):
     def test_movies_body(self):
         tester = app.test_client(self)
         response = tester.get('/movies', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'<h1>Your movie:</h1>' in response.data)
+        self.assertTrue(b'<div class="content">' in response.data)
 
     def test_return_movie(self):
         movie = return_movie()
@@ -19,21 +21,3 @@ class ContentOfMovies(unittest.TestCase):
 
 if __name__ == "__main__":
    unittest.main()
-
-
-
-# import psycopg2
-
-# def reset_movies_table():
-#     with open('../seeds/movies_seeds.sql', 'r') as seed_file:
-#         seed_sql = seed_file.read()
-#     connection = psycopg2.connect(host='127.0.0.1', dbname='whistlejacket-randomizer')
-#     cursor = connection.cursor()
-#     cursor.execute(seed_sql)
-#     connection.commit()
-#     cursor.close()
-#     connection.close()
-
-# class TestExample(unittest.TestCase):
-#     def setUp(self):
-#         reset_movies_table()
